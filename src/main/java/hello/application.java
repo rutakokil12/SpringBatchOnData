b117880f8933787a274221fcfa22d5fe599e5928
Ruta Kokil <ruta.kokil@gmail.com>
2018-04-26 14:15:13 -0400

    First commit

---
+++ src/main/java/hello/application.java
@@ -0,0 +1,10 @@
+package hello;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(application.class, args);
+    }
+}
